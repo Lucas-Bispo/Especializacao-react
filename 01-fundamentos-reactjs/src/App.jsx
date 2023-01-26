f736a2b0fe17cf8e586f1cbf3a029c64cd1106c4
Lucas-Bispo <lukaobispo@gmail.com>
2023-01-26 16:02:23 -0300

    :construction: Trabalho em progresso.

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,23 @@
+// JSX = JavaScript + XML
+import { Post } from '../src/Post';
+// Devesse de importar as funcoes dentro o app
+// Propriedades ou atributos, sao elementos de cada teg
 
-
-function App() {
-  
+export function App() {
+  // Sintax correta
   return (
-    <h1>Hell World</h1>
+    <div> 
+    <Post 
+      author="Lucas Bispo"
+      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus in magnam veniam dicta assumenda. Incidunt rerum voluptates fugit velit pariatur consequuntur aspernatur perferendis porro, ut quo laborum soluta atque repellendus."   
+    />
+
+    <Post 
+      author="Lucas Oliveira"
+      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus in magnam veniam dicta assumenda. Incidunt rerum voluptates fugit velit pariatur consequuntur aspernatur perferendis porro, ut quo laborum soluta atque repellendus."   
+    />
+    
+    </div>
   )
 }
 
-export default App
--- 01-fundamentos-reactjs/src/main.jsx
+++ 01-fundamentos-reactjs/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react' // pacote Coração que tem funcionalidades para tudo, MOBILE, TV, WEB..
 import ReactDOM from 'react-dom/client' // integração do coração com hmtl 
-import App from './App'
-import './index.css'
+import { App } from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(// Função importada da DOM para criar o html a partir de uma raiz
   // Metodo RENDER garante a renderização da funções que retornam html ou html puro
