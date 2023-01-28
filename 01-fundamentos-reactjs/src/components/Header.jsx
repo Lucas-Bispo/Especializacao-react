dde0a6c1b968344f9232875ff10d2ddfd3cdc912
Lucas-Bispo <lukaobispo@gmail.com>
2023-01-28 16:34:41 -0300

    :memo: Adicionei arquivo de logo e documentação teorica.

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,9 +1,18 @@
 //Vite tem suporte a fazer css com modulos
 import styles from '../components/Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg';// Importando logo e setando o no Html atraves de encapsulamento
+//Pois o valor fica aleatorio e segundo a sintaxy 
+
+
+
 export function Header(){// Todos os componentes devem iniciar com letra maiuscula
     return (
         <header className={styles.header}>
+            
+            <img src={igniteLogo} alt="Logo tipo do ignite" />
+            
+            
             <strong >Ignete Feed</strong>
         
         </header>
