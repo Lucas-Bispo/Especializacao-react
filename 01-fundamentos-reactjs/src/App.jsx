e2879e878f52efb2b5fcd440d6958e99eee27603
Lucas-Bispo <lukaobispo@gmail.com>
2023-01-29 13:44:33 -0300

    :memo: Comentando codigo para fixar o aprendizado.

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,6 +4,8 @@ import { Header } from './components/Header';
 // Devesse de importar as funcoes dentro o app
 // Propriedades ou atributos, sao elementos de cada teg
 import './global.css';
+import styles from './App.module.css';
+
 import { Sidebar } from './components/Sidebar';
 
 
@@ -19,13 +21,10 @@ export function App() {
       <Sidebar />
 
       <main>
-
-
       <Post 
       author="Lucas Bispo"
       content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus in magnam veniam dicta assumenda. Incidunt rerum voluptates fugit velit pariatur consequuntur aspernatur perferendis porro, ut quo laborum soluta atque repellendus."   
       />
-
       <Post 
       author="Lucas Oliveira"
       content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus in magnam veniam dicta assumenda. Incidunt rerum voluptates fugit velit pariatur consequuntur aspernatur perferendis porro, ut quo laborum soluta atque repellendus."   
