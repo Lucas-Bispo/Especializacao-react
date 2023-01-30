82d4d6603ad4a5ee6c3577e34f062798ecf24246
Lucas-Bispo <lukaobispo@gmail.com>
2023-01-30 09:39:31 -0300

    :construction: Trabalho em progresso.

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,8 @@
-import styles from '../Sidebar.module.css';
+import {PencilLine} from 'phosphor-react';
+
+
+
+import styles from './Sidebar.module.css';
 
 
 
@@ -7,15 +11,25 @@ export function Sidebar(){
         <aside className={styles.sidebar}> 
             <img 
                 className={styles.cover} 
-                src="https://images.unsplash.com/photo-1510511459019-5dda7724fd87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+                src="https://images.unsplash.com/photo-1510511459019-5dda7724fd87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
             
             <div className={styles.profile}>
+
+                <img className={styles.avatar} src="https://github.com/Lucas-Bispo.png" />
+
                 <strong>Lucas Bispo</strong>
                 <span>Web Developer</span>
             </div>
 
+
+
             <footer>
-                <a href="#">Editar seu Perfil</a>
+                
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu Perfil
+                </a>
             </footer>
         </aside>
 
