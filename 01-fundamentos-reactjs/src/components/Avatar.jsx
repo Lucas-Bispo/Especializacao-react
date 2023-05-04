656a9bc76400f4f982a9f995e4d1185dd40f1354
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-03 22:44:49 -0300

    :memo: Adicione ou atualize a documentação.

--- 01-fundamentos-reactjs/src/components/Avatar.jsx
+++ 01-fundamentos-reactjs/src/components/Avatar.jsx
@@ -1,16 +1,11 @@
-import styles from './Avatar.module.css';
+import styles from './Avatar.module.css'; // Importa um arquivo CSS específico para o componente Avatar e atribui a uma constante "styles".
 
-export function Avatar({ hasBorder = true, src }) {
-
+export function Avatar({ hasBorder = true, src }) { // Define uma função chamada Avatar que recebe dois parâmetros: hasBorder e src. O parâmetro hasBorder tem o valor padrão de true.
 
     return (
-
-        <img 
-        className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-        src={src} 
+        <img // Retorna uma imagem HTML
+        className={hasBorder ? styles.avatarWithBorder : styles.avatar} // Atribui uma classe CSS à imagem com base no valor de hasBorder. Se for verdadeiro, a classe será "avatarWithBorder"; caso contrário, a classe será "avatar".
+        src={src} // Atribui a propriedade "src" da imagem com o valor passado como argumento para a função.
         />
-    
     );
-
-    
 }
