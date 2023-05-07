d08c8ef0d544f8f7b6129730b85e1699c1f7d30b
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-07 12:08:25 -0300

    :bulb: Adicione ou atualize comentários no código-fonte.

--- 01-fundamentos-reactjs-ts/src/main.tsx
+++ 01-fundamentos-reactjs-ts/src/main.tsx
@@ -1,15 +1,12 @@
-import React from 'react' // pacote Coração que tem funcionalidades para tudo, MOBILE, TV, WEB..
-import ReactDOM from 'react-dom/client' // integração do coração com hmtl 
-import { App } from './App';
+import React from 'react' // Importa o pacote React, que fornece funcionalidades para construir interfaces de usuário.
+import ReactDOM from 'react-dom/client' // Importa a integração do React com a DOM do navegador.
 
+import { App } from './App'; // Importa o componente App do arquivo App.js.
 
-ReactDOM.createRoot(document.getElementById('root')!).render(// Função importada da DOM para criar o html a partir de uma raiz
-  // Metodo RENDER garante a renderização da funções que retornam html ou html puro
-  // tudo isso a partir do JavaScripty
-  
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-
-
+ReactDOM.createRoot(document.getElementById('root')!).render( // Cria uma raiz para renderização do componente App no DOM do navegador.
+  //* <React.StrictMode> Componente que ativa verificações extras em tempo de desenvolvimento e sinaliza o uso de recursos obsoletos.
+  // <App /> Renderiza o componente App importado anteriormente.
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode> 
 )
