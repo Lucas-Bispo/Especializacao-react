455e559509cfb07fe086808a34617117cf2c7257
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-07 23:13:21 -0300

    :bulb: Adicione ou atualize comentários no código-fonte.

--- 01-fundamentos-reactjs-ts/src/App.tsx
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -1,3 +1,7 @@
+/*Aqui estamos importando módulos do nosso projeto. O Header, Post e Sidebar são componentes que estão 
+localizados na pasta components. O styles é um objeto que representa a classe CSS definida no arquivo 
+App.module.css e o arquivo global.css contém estilos globais que serão aplicados a todo o projeto.
+ */
 import { Header } from './components/Header';
 import { Post, PostType } from './components/Post'
 import { Sidebar } from './components/Sidebar';
@@ -5,6 +9,10 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 
 import './global.css';
+
+/* Aqui estamos criando uma variável posts do tipo PostType[] que é uma interface que define o formato de um post. 
+ * Essa variável contém um array de objetos que representam cada post, com propriedades como id, autor, conteúdo e data de publicação.
+*/
 
 const posts: PostType[] = [
   {
@@ -36,7 +44,13 @@ const posts: PostType[] = [
     publishedAt: new Date('2022-05-10 20:00:00'),
   },
 ];
-
+/**
+ * 
+ * A função "App" é exportada para ser utilizada em outras partes do código.
+  A função retorna uma estrutura HTML, que é composta pelo componente Header, Sidebar, main e o array de posts. 
+  A classe "wrapper" é adicionada à div que contém o Sidebar e o main, que possuem os posts.
+  O array de posts é mapeado e cada objeto é renderizado no componente Post, passando o id e o conteúdo do post.
+ */
 export function App() {
   return (
     <div>
