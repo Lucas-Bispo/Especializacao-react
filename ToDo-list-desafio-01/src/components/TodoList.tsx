0e26da42add2bc627a72044c8e6994d9e7094c21
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-11 21:54:58 -0300

    :sparkles: Apresentar novos recursos.

---
+++ ToDo-list-desafio-01/src/components/TodoList.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import styles from './ToDoList.module.css';
+
+function TodoList() {
+    /* Criar um componente funcional TodoList que representará a lista de tarefas. 
+     * Dentro dele, vamos criar um estado tasks que será um array de objetos contendo 
+     * a descrição da tarefa e um booleano indicando se a tarefa foi concluída ou não.
+     */
+  const [tasks, setTasks] = useState<{ description: string; completed: boolean }[]>([]);
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+  
+  /*
+    Dentro do componente TodoList, vamos criar um estado newTaskDescription que representará 
+    o texto digitado pelo usuário na entrada de texto. Também vamos criar uma função handleNewTaskDescriptionChange 
+    que será chamada sempre que o texto for modificado.
+    */
+  function handleNewTaskDescriptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTaskDescription(event.target.value);
+   
+  }
+   /**
+     * Agora precisamos criar a lógica para adicionar uma nova tarefa ao clicar no botão. Para isso, vamos criar uma função
+     *  handleAddTask que será chamada quando o usuário clicar no botão. Dentro dela, vamos criar uma nova tarefa com o texto 
+     * digitado e adicioná-la ao estado tasks. Por fim, vamos limpar a entrada de texto.
+     */
+   function handleAddTask() {
+    const newTask = { description: newTaskDescription, completed: false };
+    setTasks([...tasks, newTask]);
+    setNewTaskDescription("");
+  }
+
+  return (
+    <div>
+      {/* Entrada de texto */}
+      <input type="text" value={newTaskDescription} onChange={handleNewTaskDescriptionChange} />
+      {/* Botão para adicionar tarefa */}
+      <button onClick={handleAddTask}>Adicionar</button>
+      {/* Lista de tarefas */}
+      {/* Campos de estatísticas */}
+    </div>
+  );
+}
+
+export default TodoList;
