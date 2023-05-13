8d596259601cbe49c5ce3c2a68765a064898213c
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-13 09:02:45 -0300

    :boom: Introduza mudanças importantes.

--- ToDo-list-desafio-01/src/App.tsx
+++ ToDo-list-desafio-01/src/App.tsx
@@ -1,14 +1,14 @@
 import { Header } from "./components/Header";
 import { TodoList } from "./components/TodoList";
 
-//import styles from './App.module.css'; <Header />
+//import styles from './App.module.css'; 
 import './global.css';
 
 
 export function App(){
     return(
         <div>
-            
+            <Header />
             <TodoList />
         </div>
     );
