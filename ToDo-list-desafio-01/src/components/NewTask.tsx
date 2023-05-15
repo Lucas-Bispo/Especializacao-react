4490fc975c5af56a2de814dc1dffc83bf19137e9
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-14 21:18:21 -0300

    :bulb:Adicione ou atualize comentários no código-fonte.

--- ToDo-list-desafio-01/src/components/NewTask.tsx
+++ ToDo-list-desafio-01/src/components/NewTask.tsx
@@ -1,81 +1,98 @@
-import styles from './NewTask.module.css'
-import { Task } from './Task'
-import { PlusCircle } from 'phosphor-react'
-import { ChangeEvent, FormEvent, InvalidEvent, useEffect, useState } from 'react'
-import { NoTask } from './NoTask'
+// Importando módulos
+import styles from './NewTask.module.css';
+import { Task } from './Task';
+import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, InvalidEvent, useEffect, useState } from 'react';
+import { NoTask } from './NoTask';
 
+// Interface da tarefa
 interface Task {
-  id: number
-  content: string
-  isChecked: boolean
+  id: number;
+  content: string;
+  isChecked: boolean;
 }
 
-
+// Componente principal
 export function NewTask() {
   
+  // Estado inicial das tarefas
   const [tasks, setTasks] = useState<Task[]>(() => {
-    const storageTask = localStorage.getItem('@taskList')
+    // Recuperando tarefas armazenadas no LocalStorage
+    const storageTask = localStorage.getItem('@taskList');
     if(storageTask) {
-      return JSON.parse(storageTask)
+      return JSON.parse(storageTask);
     }
-    return []
-  })
-  const [newTaskValue, setNewTaskValue] = useState('')
-  const [checkedCount, setCheckedCount] = useState(0)
+    return [];
+  });
 
-  useEffect(()=>{
-    localStorage.setItem('@taskList', JSON.stringify(tasks))
-  },[tasks])
+  // Estado para armazenar o valor da nova tarefa
+  const [newTaskValue, setNewTaskValue] = useState('');
 
+  // Estado para armazenar o número de tarefas concluídas
+  const [checkedCount, setCheckedCount] = useState(0);
+
+  // Efeito colateral para salvar tarefas no LocalStorage toda vez que houver uma alteração
+  useEffect(() => {
+    localStorage.setItem('@taskList', JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Função para adicionar uma nova tarefa
   function handleAddNewTask(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
     const newTask = {
       id: Math.floor(Math.random() * 999999 + 1),
       content: newTaskValue,
       isChecked: false
-    }
-    setTasks([...tasks, newTask])
-    setNewTaskValue('')
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskValue('');
   }
 
+  // Função para atualizar o estado da nova tarefa toda vez que houver uma mudança no input
   function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>){
-    event.target.setCustomValidity('')
-    setNewTaskValue(event.target.value)
+    event.target.setCustomValidity('');
+    setNewTaskValue(event.target.value);
   }
 
+  // Função para exibir mensagem de erro caso o campo esteja inválido
   function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>){
     event.target.setCustomValidity('Esse campo é obrigatório');
   }
   
+  // Função para deletar uma tarefa
   function deleteTask(taskToDelete: number){
     const taskWithoutDeleteOne = tasks.filter(task=>{
       if(task.isChecked === true) {
-        setCheckedCount(checkedCount - 1)
+        setCheckedCount(checkedCount - 1);
       }
       return (
-        task.id !== taskToDelete)
-      })
-    setTasks(taskWithoutDeleteOne)
+        task.id !== taskToDelete
+      );
+    });
+    setTasks(taskWithoutDeleteOne);
   }
 
+  // Função para atualizar o estado de isChecked ao clicar no checkbox
   function handleCheckboxChange(id: number) {
     const taskComplete = tasks.map(task=>{
-      if(task.id===id) task.isChecked = !task.isChecked
-      return task
-    })
-    setTasks(taskComplete)
+      if(task.id===id) task.isChecked = !task.isChecked;
+      return task;
+    });
+    setTasks(taskComplete);
   }
 
+  // Função para atualizar o estado do contador de tarefas concluídas
   function handleCompleteProgress(progress: number | boolean ) {
       if(!progress) {
-        setCheckedCount(checkedCount + 1)
+        setCheckedCount(checkedCount + 1);
       } else {
-        setCheckedCount(checkedCount - 1)
+        setCheckedCount(checkedCount - 1);
       }
   }
 
-  return(
+  return (
     <>
+      {/* Formulário para adicionar nova tarefa */}
       <form className={styles.newTask} onSubmit={handleAddNewTask}>
         <input 
           type="text" 
@@ -91,14 +108,23 @@ export function NewTask() {
           <PlusCircle size={16} weight="bold" />
         </button>
       </form>
-
+  
+      {/* Seção para exibir contadores de tarefas */}
       <section className={styles.containerCount}>
         <p>Tarefas criadas<span>{tasks.length}</span></p>
         <p>Concluídas
-          {tasks.length === 0 ? <span>{tasks.length}</span> : <span>{checkedCount} de {tasks.length}</span>}
+          {/* Verifica se há tarefas concluídas e exibe o contador */}
+          {tasks.length === 0 ? 
+            <span>{tasks.length}</span> : 
+            <span>{checkedCount} de {tasks.length}</span>
+          }
         </p>
       </section>
-      {tasks.length === 0 ? <NoTask/> : 
+  
+      {/* Verifica se há tarefas a serem exibidas */}
+      {tasks.length === 0 ? 
+        <NoTask/> : 
+        // Se houver tarefas, exibe a lista de tarefas
         <section className={styles.taskList}> {tasks.map(task => {
           return (
             <Task 
--- ToDo-list-desafio-01/src/components/NoTask.tsx
+++ ToDo-list-desafio-01/src/components/NoTask.tsx
@@ -1,14 +1,13 @@
-import styles from './NoTask.module.css'
-import { ClipboardText } from 'phosphor-react'
+import styles from './NoTask.module.css'; // Importa o arquivo CSS local para estilização
+import { ClipboardText } from 'phosphor-react'; // Importa o ícone ClipboardText da biblioteca Phosphor React
 
 export function NoTask() {
-
   return(
-    <div className={styles.noTask}>
-      <ClipboardText size={56} color="var(--gray-300)"/>
-      <div className={styles.content}>
-        <strong>Você ainda não tem tarefas cadastradas</strong>
-        <p>Crie tarefas e organize seus itens a fazer</p>
+    <div className={styles.noTask}> {/* Define a classe CSS para o contêiner da div */}
+      <ClipboardText size={56} color="var(--gray-300)"/> {/* Renderiza o ícone ClipboardText com tamanho 56 e cor cinza */}
+      <div className={styles.content}> {/* Define a classe CSS para o contêiner da div */}
+        <strong>Você ainda não tem tarefas cadastradas</strong> {/* Renderiza um texto em negrito indicando que não há tarefas */}
+        <p>Crie tarefas e organize seus itens a fazer</p> {/* Renderiza um parágrafo sugerindo que o usuário crie tarefas */}
       </div>
     </div>
   );
