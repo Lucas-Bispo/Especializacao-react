082dd2ef498bb6190b1fdd8b7e3919a9c9a52ca8
Lucas-Bisop <lukaobispo@gmail.com>
2023-05-20 22:10:51 -0300

    :construction: Estilos globais Cores & fonte Configurando ESLint

--- 02-Ignite-Timer/src/components/Button.styles.ts
+++ 02-Ignite-Timer/src/components/Button.styles.ts
@@ -1,24 +1,24 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
-  variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
+  ${(props) => {
     return css`
-      background-color: ${buttonVariants[props.variant]}
+      background-color: ${buttonVariants[props.variant]};
     `
   }}
-`;
+`
