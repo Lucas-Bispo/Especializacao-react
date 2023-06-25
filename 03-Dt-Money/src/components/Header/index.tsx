d8084185e438d40269e126c44fb133f536370dea
Lucas-Bisop <lukaobispo@gmail.com>
2023-06-25 00:34:16 -0300

    :lipstick:Estrutura visual Botões de entrada e saída

--- 03-Dt-Money/src/components/Header/index.tsx
+++ 03-Dt-Money/src/components/Header/index.tsx
@@ -3,27 +3,20 @@ import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from "../../assets/logo.svg";
 
+import { NewTransactionModal } from "../NewTransactionModal";
+
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-
-
-        <Dialog.Root>
+      <Dialog.Root>
         <Dialog.Trigger asChild>
           <NewTransactionButton>Nova transação</NewTransactionButton>
         </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay />
-
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
+        <NewTransactionModal />
       </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
