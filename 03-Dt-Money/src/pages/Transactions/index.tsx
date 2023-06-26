3813d98921397f036ab3b2e4ce22c856fe76c8b4
Lucas-Bisop <lukaobispo@gmail.com>
2023-06-25 23:56:03 -0300

    :building_construction: Conexão com a API - Criando hook para o resumo

--- 03-Dt-Money/src/pages/Transactions/index.tsx
+++ 03-Dt-Money/src/pages/Transactions/index.tsx
@@ -1,25 +1,23 @@
 import { useContext } from "react";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { SearchForm } from "./components/SearchForm";
+import { Header } from "../../components/Header/index.tsx";
+import { TransactionsContext } from "../../contexts/TransactionsContext.tsx";
+import { Summary } from "../../components/Summary/index.tsx";
+import { SearchForm } from "../../components/SearchForm/index.tsx";
+import { priceFormatter, dateFormatter } from "../../utils/formatter.ts";
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles.ts";
 
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext)
-
   return (
     <div>
       <Header />
       <Summary />
-
       <TransactionsContainer>
         <SearchForm />
-
         <TransactionsTable>
           <tbody>
-            {transactions.map((transaction) => {
+            {transactions.map(transaction => {
               return (
                 <tr key={transaction.id}>
                   <td width="50%">{transaction.description}</td>
@@ -30,9 +28,7 @@ export function Transactions() {
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>
-                    {dateFormatter.format(new Date(transaction.createdAt))}
-                  </td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
@@ -40,5 +36,5 @@ export function Transactions() {
         </TransactionsTable>
       </TransactionsContainer>
     </div>
-  )
+  );
 }
