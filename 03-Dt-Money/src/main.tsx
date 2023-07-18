8538676eaf2bf5418dea021e7e8208913af5b5b6
Lucas-Bisop <lukaobispo@gmail.com>
2023-07-17 23:00:02 -0300

    :construction: Trabalho em progresso.

--- 03-Dt-Money/src/main.tsx
+++ 03-Dt-Money/src/main.tsx
@@ -1,11 +1,15 @@
+// Importa o React, ReactDOM e o componente App
 import React from "react";
+// Importa o ReactDOM, que é uma biblioteca JavaScript para renderizar componentes React no DOM.
 import ReactDOM from "react-dom";
+// Importa o componente App, que é o componente que será renderizado na página.
 import { App } from "./App";
 
-
-
+// Cria uma nova instância do ReactDOM e renderiza o componente App na raiz da página.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    // Inicia o modo estrito do React, que ajuda a detectar erros no código React.
     <App />
+    // Renderiza o componente App na página.
   </React.StrictMode>,
-)
+);
