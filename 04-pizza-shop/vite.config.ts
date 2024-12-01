c49b55de75d3db783c7e5ac75c574ab220001e70
Lucas-Bispo <lukaobispo@gmail.com>
2024-12-01 11:58:56 -0300

    :zap: Voltando a Ativa

--- 04-pizza-shop/vite.config.ts
+++ 04-pizza-shop/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./src/test/setup.ts'],
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
