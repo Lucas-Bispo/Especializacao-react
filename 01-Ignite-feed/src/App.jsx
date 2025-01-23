44a6eaa6656f19f1eaf2fe5a216982225c197ea8
Lucas-Bispo <lukaobispo@gmail.com>
2025-01-22 21:28:24 -0300

    :memo: Documentando Aplicacao

--- 01-Ignite-feed/src/App.jsx
+++ 01-Ignite-feed/src/App.jsx
@@ -1,22 +1,36 @@
-// Importa o componente Post do arquivo Post.js ou Post.jsx.
-// Esse componente será usado dentro do componente App.
-import Post from "./Post";
+// Importação do componente Post do arquivo './Post'.
+// Este componente será utilizado dentro do componente App para exibir informações sobre posts.
+import { Post } from './Post';
 
-// Define o componente principal da aplicação, chamado App.
-function App() {
+// Importa o arquivo de estilos globais chamado "global.css".
+// Este arquivo contém estilos CSS que serão aplicados globalmente à aplicação,
+// afetando todos os componentes e elementos, a menos que sejam sobrescritos por estilos específicos.
+import './global.css';
+
+
+// Exportação da função App, que representa o componente principal da aplicação.
+export function App() {
+  // O componente App retorna um elemento JSX que representa a estrutura da interface.
   return (
-      // Retorna um elemento <div> que agrupa vários componentes Post.
-      <div>
-          {/* Renderiza o componente Post com uma propriedade "content" definida como "Post 1". */}
-          <Post content="Post 1" />
-          {/* Renderiza outro componente Post com a propriedade "content" definida como "Post 2". */}
-          <Post content="Post 2" />
-          {/* Renderiza um terceiro componente Post com a propriedade "content" definida como "Post 3". */}
-          <Post content="Post 3" />
-      </div>
+    <div>
+      {/* Instância do componente Post com duas propriedades (props): 
+          - author: Representa o nome do autor do post.
+          - content: Representa o conteúdo textual do post.
+          Este componente será renderizado com base nas informações fornecidas nas props. */}
+      <Post
+        author="Diego Fernandes"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      
+      {/* Outra instância do componente Post, mas com um autor e conteúdo diferentes. 
+          Isso mostra como o mesmo componente pode ser reutilizado com diferentes dados. */}
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
+    </div>
   );
 }
 
-// Exporta o componente App como a exportação padrão deste módulo.
-// Outros arquivos podem importar e usar o App diretamente, por exemplo: import App from './App';
-export default App;
+// Este código exporta o componente App, permitindo que ele seja utilizado em outros arquivos,
+// como no ponto de entrada da aplicação (geralmente no arquivo main.jsx).
