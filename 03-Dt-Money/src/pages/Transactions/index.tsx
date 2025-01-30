57693e441e8d44f239d13a82947cbe7636109b4f
Lucas-Bispo <lukaobispo@gmail.com>
2025-01-30 20:24:29 -0300

    :construction:Tabela de transações

--- 03-Dt-Money/src/pages/Transactions/index.tsx
+++ 03-Dt-Money/src/pages/Transactions/index.tsx
@@ -2,6 +2,8 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
 // Define e exporta o componente funcional Transactions
 export function Transactions() {
   return (
@@ -11,6 +13,34 @@ export function Transactions() {
       <Header />
 
       <Summary/>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  -R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
 
     </div>
   );
