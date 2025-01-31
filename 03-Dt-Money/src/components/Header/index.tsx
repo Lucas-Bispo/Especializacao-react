ef1d2990272bdd49dea0f4ded547b672154c5c13
Lucas-Bispo <lukaobispo@gmail.com>
2025-01-31 19:10:18 -0300

    :rocket: Criando um modal acessível

--- 03-Dt-Money/src/components/Header/index.tsx
+++ 03-Dt-Money/src/components/Header/index.tsx
@@ -1,22 +1,27 @@
-// Importando os componentes estilizados de um arquivo de estilos (presumivelmente com styled-components)
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
-// Importando a imagem do logo que será exibida no cabeçalho
 import logoImg from "../../assets/logo.svg";
 
-// Definindo o componente funcional Header
 export function Header() {
-  // Retorna a estrutura do cabeçalho
   return (
-    // HeaderContainer é o componente que pode conter o layout geral do cabeçalho
     <HeaderContainer>
-      {/* HeaderContent pode ser usado para agrupar os itens dentro do cabeçalho */}
       <HeaderContent>
-        {/* A imagem do logo é exibida aqui. A propriedade 'src' aponta para o caminho da imagem importada */}
         <img src={logoImg} alt="" />
-        {/* O botão de nova transação é exibido aqui com o texto "Nova transação" */}
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay />
+          <Dialog.Content>
+            <Dialog.Title>Nova Transação</Dialog.Title>
+            <Dialog.Close />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  );
+  )
 }
