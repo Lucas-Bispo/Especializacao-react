bdda6438733012bd243462f64fb2d088e20d585f
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-03 15:47:05 -0300

    :construction: Buscando transações

--- 03-Dt-Money/src/contexts/TransactionsContext.tsx
+++ 03-Dt-Money/src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
+
 interface Transaction {
   id: number;
   description: string;
@@ -7,26 +8,43 @@ interface Transaction {
   category: string;
   createdAt: string;
 }
+
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
+
 interface TransactionsProviderProps {
   children: ReactNode
 }
+
 export const TransactionsContext = createContext({} as TransactionContextType);
+
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
+
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3333/transactions');
+    if (query) {
+      url.searchParams.append('q', query);
+    }
+    const response = await fetch(url)
     const data = await response.json()
+
     setTransactions(data)
   }
+
   useEffect(() => {
-    loadTransactions()
+    fetchTransactions()
   }, []);
+
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{
+      transactions,
+      fetchTransactions
+    }}>
       {children}
     </TransactionsContext.Provider>
-  );
+  )
+
 }
