84e2df48fe776e6f37ef331fb0e0888dc592fae9
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-05 17:34:10 -0300

    :construction: Estado (useState)

--- 01-Ignite-feed/src/components/Post.jsx
+++ 01-Ignite-feed/src/components/Post.jsx
@@ -1,19 +1,30 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
+
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true
   });
 
+  function handleCrateNewComment() {
+    event.preventDefault()
+    setComments([...comments, comments.length + 1]);
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -40,7 +51,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
@@ -53,9 +64,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
