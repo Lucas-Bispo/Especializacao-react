6abd6f759f05065ebf5a6cf2a9436c4f52cbc61f
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-05 19:04:14 -0300

    :construction: Comunicação entre componentes

--- 01-Ignite-feed/src/components/Post.jsx
+++ 01-Ignite-feed/src/components/Post.jsx
@@ -34,6 +34,9 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -77,7 +80,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
