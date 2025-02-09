c99f3f6e1558c191ad2d63b9f70d9014824902a5
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-08 21:12:00 -0300

    :rocket: Tas

--- Desafios/01-todo-list/src/components/Task/index.tsx
+++ Desafios/01-todo-list/src/components/Task/index.tsx
@@ -1,25 +1,21 @@
-import { TbTrash } from 'react-icons/tb';
-import styles from './task.module.css'
+import { TbTrash } from 'react-icons/tb'; // Importa o ícone de lixeira
+import styles from './task.module.css'; // Importa os estilos CSS específicos para o componente Task
 
-
+// Define o componente funcional Task
 export function Task() {
     return (
-        <div className={styles.task}>
-        <button className={styles.checkContainer}>
+        <div className={styles.task}> {/* Container principal da tarefa com estilos aplicados */}
+            <button className={styles.checkContainer}> {/* Botão para marcar a tarefa como concluída */}
+                <div></div> {/* Indicador visual dentro do botão */}
+            </button>
 
-            <div></div>
+            <p> {/* Texto da tarefa */}
+                Integrar 
+            </p>
 
-        </button>
-
-        <p>
-            Integrar 
-        </p>
-
-        <button className={styles.deleteButton}>
-            <TbTrash size={20} />
-        </button>
-
-
+            <button className={styles.deleteButton}> {/* Botão para deletar a tarefa */}
+                <TbTrash size={20} /> {/* Ícone de lixeira com tamanho de 20px */}
+            </button>
         </div>
     );
 }
