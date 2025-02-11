d0a92ffb7083610366e626353227399113b11b63
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-11 09:10:07 -0300

    :building_construction: Fazer mudanças arquitetônicas.

--- ReactJs/02-Ignite-timer/src/App.tsx
+++ ReactJs/02-Ignite-timer/src/App.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from './components/Button';
-import defaultTheme from "./components/styles/themes/default";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 
 export function App() {
@@ -11,6 +13,7 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
--- ReactJs/02-Ignite-timer/src/components/styles/themes/default.ts
+++
@@ -1,7 +0,0 @@
-const defaultTheme = {
-    primary: '#8257E6',
-    white: '#fffff',
-    black: '#000000',
-}
-
-export default defaultTheme;
