41b7c60c717c7956dcf1c55e2e4a74a99ed4bb4b
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-19 08:43:42 -0300

    :construction: Realizando queries com Knex

--- Node/02-API-REST-NODEJS/src/server.ts
+++ Node/02-API-REST-NODEJS/src/server.ts
@@ -1,24 +1,21 @@
-import fastify from "fastify";
-import { knex } from "./database";
 
-// Cria um servidor Fastify
-const app = fastify();
+import fastify from 'fastify'
+import { knex } from './database'
 
-// Cria uma rota GET para a URL '/hello'
+const app = fastify()
+
 app.get('/hello', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  // Executa uma consulta no banco de dados para obter todas as tabelas
-  const tables = await knex('sqlite_schema').select('*')
-
-  // Retorna as tabelas como resposta da API
-  return tables
-
-  // Caso queira, pode ser retornado um simples "Hello World"
-  //return 'Hello World';
-});
-
-// Inicia o servidor
-app.listen({ port: 3333}).then(() => {
-  // Imprime uma mensagem no console para informar que o servidor est  rodando
-  console.log('HTTP Server running on http://localhost:3333')
+  return transactions
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
