cebc3725a2b64a51ff0d7ab9f3fd16de1da16c42
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-18 23:32:55 -0300

    :construction: Criando tabela de transações

--- Node/02-API-REST-NODEJS/src/database.ts
+++ Node/02-API-REST-NODEJS/src/database.ts
@@ -1,9 +1,14 @@
-import { knex as setupKnex } from 'knex'
+import { Knex, knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
-    client: 'sqlite3',
-    connection: {
-        filename: './tmp/app.db',
-    },
-   // useNullAsDefault: true,
-})
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+export const knex = setupKnex(config)
--- Node/02-API-REST-NODEJS/src/server.ts
+++ Node/02-API-REST-NODEJS/src/server.ts
@@ -1,18 +1,24 @@
 import fastify from "fastify";
 import { knex } from "./database";
 
+// Cria um servidor Fastify
 const app = fastify();
+
+// Cria uma rota GET para a URL '/hello'
 app.get('/hello', async () => {
 
-  const tables = knex('sqlite_schema').select('*')
+  // Executa uma consulta no banco de dados para obter todas as tabelas
+  const tables = await knex('sqlite_schema').select('*')
 
+  // Retorna as tabelas como resposta da API
   return tables
 
+  // Caso queira, pode ser retornado um simples "Hello World"
   //return 'Hello World';
 });
 
-//iniciar servidor
-
+// Inicia o servidor
 app.listen({ port: 3333}).then(() => {
+  // Imprime uma mensagem no console para informar que o servidor est  rodando
   console.log('HTTP Server running on http://localhost:3333')
 })
