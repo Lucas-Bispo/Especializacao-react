7709e1a898842ca09578b83bfaa6393beea3ee5b
Lucas-Bispo <lukaobispo@gmail.com>
2025-02-21 10:47:24 -0300

    :zap: Preparando para deploy

---
+++ Node/02-API-REST-NODEJS/dist/database.js
@@ -0,0 +1,48 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/database.ts
+var database_exports = {};
+__export(database_exports, {
+  config: () => config,
+  knex: () => knex
+});
+module.exports = __toCommonJS(database_exports);
+var import_config = require("dotenv/config");
+var import_knex = require("knex");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env not found.");
+}
+var config = {
+  client: "sqlite",
+  connection: {
+    filename: process.env.DATABASE_URL
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations"
+  }
+};
+var knex = (0, import_knex.knex)(config);
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  config,
+  knex
+});
