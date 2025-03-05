e494d480c1e2da7c26ae20af82db8aa35c2845ce
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-05 19:41:59 -0300

    :ghost: Trabalho em construcao

--- Node/03-API-SOLID/src/http/controllers/register.ts
+++ Node/03-API-SOLID/src/http/controllers/register.ts
@@ -1,35 +1,58 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+
 import { z } from 'zod'
-import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { UserAlreadyExistsError } from '@/use-cases/erros/user-already-exists-error'
 
 
+
 export async function register(request: FastifyRequest, reply: FastifyReply) {
+
   const registerBodySchema = z.object({
+
     name: z.string(),
+
     email: z.string().email(),
+
     password: z.string().min(6),
+
   })
+
+
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+
+
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+
+
+    const registerUseCase = makeRegisterUseCase()
+
+
+
 
     await registerUseCase.execute({
-      name,
-      email,
-      password,
-    })
-  } catch (err) {
-    if (err instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: err.message })
-    }
 
+      name,
+
+      email,
+
+      password,
+
+    })
+
+  } catch (err) {
+
+    if (err instanceof UserAlreadyExistsError) {
+
+      return reply.status(409).send({ message: err.message })
+
+    }
     throw err
   }
-
   return reply.status(201).send()
+
 }
---
+++ Node/03-API-SOLID/src/use-cases/erros/resource-not-found-error.ts
@@ -0,0 +1,13 @@
+export class ResourceNotFoundError extends Error {
+
+
+    constructor() {
+  
+  
+      super('Resource not found.')
+  
+  
+    }
+  
+  
+  }
