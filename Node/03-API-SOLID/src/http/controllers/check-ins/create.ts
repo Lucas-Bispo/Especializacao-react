3e6847ea0c2adbc7a9beb9b6cf81a06a4f252e86
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-06 11:52:18 -0300

    :alien: Mudanca Sugerida pela IA

--- Node/03-API-SOLID/src/http/controllers/check-ins/create.ts
+++ Node/03-API-SOLID/src/http/controllers/check-ins/create.ts
@@ -2,31 +2,54 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { makeCheckInUseCase } from '@/use-cases/factories/make-check-in-use.case'
 
-export async function create(request: FastifyRequest, reply: FastifyReply) {
-  const createCheckInParamsSchema = z.object({
-    gymId: z.string().uuid(),
-  })
+interface CustomRequest extends FastifyRequest {
+  user: {
+    sub: string
+  }
+  params: {
+    gymId: string
+  }
+  body: {
+    latitude: number
+    longitude: number
+  }
+}
 
-  const createCheckInBodySchema = z.object({
+const requestSchema = z.object({
+  user: z.object({
+    sub: z.string()
+  }),
+  params: z.object({
+    gymId: z.string().uuid()
+  }),
+  body: z.object({
     latitude: z.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
     longitude: z.number().refine((value) => {
       return Math.abs(value) <= 180
-    }),
+    })
   })
+})
 
-  const { gymId } = createCheckInParamsSchema.parse(request.params)
-  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+export async function create(request: CustomRequest, reply: FastifyReply) {
+  try {
+    const validatedRequest = requestSchema.parse(request)
+    const { user, params, body } = validatedRequest
+    const { gymId } = params
+    const { latitude, longitude } = body
 
-  const checkInUseCase = makeCheckInUseCase()
+    const checkInUseCase = makeCheckInUseCase()
+    await checkInUseCase.execute({
+      gymId,
+      userId: user.sub,
+      userLatitude: latitude,
+      userLongitude: longitude,
+    })
 
-  await checkInUseCase.execute({
-    gymId,
-    userId: request.user.sub,
-    userLatitude: latitude,
-    userLongitude: longitude,
-  })
-
-  return reply.status(201).send()
+    return reply.status(201).send()
+  } catch (error) {
+    console.error(error)
+    return reply.status(500).send({ message: 'Erro interno do servidor' })
+  }
 }
