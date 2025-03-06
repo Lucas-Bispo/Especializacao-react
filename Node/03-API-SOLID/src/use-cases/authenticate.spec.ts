10428222efd82648de96dca8e7ab383eda0b28e0
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-06 10:57:12 -0300

    :construction: Trabalho em andamento

--- Node/03-API-SOLID/src/use-cases/authenticate.spec.ts
+++ Node/03-API-SOLID/src/use-cases/authenticate.spec.ts
@@ -1,12 +1,10 @@
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { hash } from 'bcryptjs'
 import { expect, describe, it, beforeEach } from 'vitest'
-import { InvalidCredentialsError } from './erros/invalid-credentials-error'
-
 
 let usersRepository: InMemoryUsersRepository
-
 let sut: AuthenticateUseCase
 
 describe('Authenticate Use Case', () => {
@@ -14,19 +12,22 @@ describe('Authenticate Use Case', () => {
     usersRepository = new InMemoryUsersRepository()
     sut = new AuthenticateUseCase(usersRepository)
   })
+
   it('should be able to authenticate', async () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
     })
+
     const { user } = await sut.execute({
-      
       email: 'johndoe@example.com',
       password: '123456',
     })
+
     expect(user.id).toEqual(expect.any(String))
   })
+
   it('should not be able to authenticate with wrong email', async () => {
     await expect(() =>
       sut.execute({
@@ -35,20 +36,19 @@ describe('Authenticate Use Case', () => {
       }),
     ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
+
   it('should not be able to authenticate with wrong email', async () => {
     await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
     })
+
     await expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
         password: '123123',
       }),
-
     ).rejects.toBeInstanceOf(InvalidCredentialsError)
-
   })
-
 })
