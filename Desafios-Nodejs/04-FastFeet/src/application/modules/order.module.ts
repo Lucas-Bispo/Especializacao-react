f5a2e4412607b49a50b892e5614332373b12b7b3
Lucas-Bispo <lukaobispo@gmail.com>
2025-03-09 09:28:48 -0300

    :wrench: CRUD de Destinatários

--- Desafios-Nodejs/04-FastFeet/src/application/modules/order.module.ts
+++ Desafios-Nodejs/04-FastFeet/src/application/modules/order.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { OrderController } from '../../infrastructure/http/controllers/order.controller';
+import { CreateOrderUseCase } from '../../domain/order/use-cases/create-order.use-case';
+import { ListOrdersUseCase } from '../../domain/order/use-cases/list-orders.use-case';
+import { UpdateOrderUseCase } from '../../domain/order/use-cases/update-order.use-case';
+import { DeleteOrderUseCase } from '../../domain/order/use-cases/delete-order.use-case';
+import { OrderRepository } from '../../domain/order/repositories/order.repository';
+import { PrismaOrderRepository } from '../../infrastructure/prisma/order-repository.prisma';
+import { PrismaService } from '../../infrastructure/prisma/prisma.service';
+import { AuthModule } from '../../infrastructure/auth/auth.module';
+import { UserRepository } from '../../domain/user/repositories/user.repository';
+import { PrismaUserRepository } from '../../infrastructure/auth/prisma-user.repository';
+import { RecipientRepository } from '../../domain/recipient/repositories/recipient.repository';
+import { PrismaRecipientRepository } from '../../infrastructure/prisma/recipient-repository.prisma';
 
-@Module({})
+@Module({
+  imports: [AuthModule],
+  controllers: [OrderController],
+  providers: [
+    CreateOrderUseCase,
+    ListOrdersUseCase,
+    UpdateOrderUseCase,
+    DeleteOrderUseCase,
+    PrismaService,
+    { provide: OrderRepository, useClass: PrismaOrderRepository },
+    { provide: UserRepository, useClass: PrismaUserRepository },
+    { provide: RecipientRepository, useClass: PrismaRecipientRepository },
+  ],
+})
 export class OrderModule {}
