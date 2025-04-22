3dc5f69ac40acfc1145ad784a03ba9bd3de9738b
Lucas-Bispo <lukaobispo@gmail.com>
2025-04-21 23:22:12 -0300

    :wrench: Setup do projeto - Instalando React Router DOM

--- ReactJs/04-Pizza-Shop/pizzashop-web/src/pages/routes.tsx
+++ ReactJs/04-Pizza-Shop/pizzashop-web/src/pages/routes.tsx
@@ -1,4 +1,10 @@
 
+import { createBrowserRouter } from 'react-router-dom'
+
+
+
+import { Dashboard } from './app/dashboard'
+import { SignIn } from './auth/sign-in'
 
 export const router = createBrowserRouter([
   {
